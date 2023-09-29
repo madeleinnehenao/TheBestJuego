80ef778fbdae03a761ed553af3ddd030428856ed
Juan Gabriel Góez Duque <jgoezd@unal.edu.co>
2023-09-28 20:40:18 -0500

    Update Manzana.py

--- Manzana.py
+++ Manzana.py
@@ -3,25 +3,34 @@ import random
 import pygame
 from pygame.math import Vector2
 
-class Manzana(pygame.sprite.Sprite):
+class Manzana:
+    """
+    Esta clase se encarga de dibujar la manzana en la pantalla, 
+    además de desplazar la manzana a una posición aleatoria dentro
+    de los límites de la pantalla
+
+    Args:
+    numero_bloque: int. Cantidad de bloques que hay en la matriz.
+    """
     def __init__(self, numero_bloque: int):
-        pygame.sprite.Sprite.__init__(self)
-        self.aparecer(numero_bloque)
+        self.desplazar(numero_bloque)
         self.visible = False
 
 
     def dibujar_manzana(self, tamano_bloque:int,
-                        pantalla:object) ->None:
-        """Crea un rectángulo y dibuja el 
-        rectángulo en la pantalla
+                        pantalla:object) -> None:
+        """
+        Crea un rectángulo y dibuja el rectángulo en la pantalla
+        simulando a una manzana.
         
-        args:
+        Args:
         tamano_bloque: Int. Es el tamaño que ocupará la manzana
         pantalla: Es un objeto de tipo surface, donde se dibujará
         la manzana
          
-        returns:
-        No hace un return específico, sólo dibuja la manzana """
+        Returns:
+        None
+        """
         manzana_rect = pygame.Rect(self.pos.x * tamano_bloque,
                                     self.pos.y * tamano_bloque,
                                     tamano_bloque, tamano_bloque)
@@ -29,15 +38,20 @@ class Manzana(pygame.sprite.Sprite):
         pygame.draw.rect(pantalla, (220, 0, 0), manzana_rect)
 
     # Generar posicion aleatoria
-    def aparecer(self, numero_bloque: int):
-        """Crea una posición aleatoria dentro de la matriz,
-        en la cual aparecerá la manzana
+    def desplazar(self, numero_bloque: int) -> None:
+        """
+        Desplaza la manzana a una posición dentro de la matriz.
         
-        args:
-        numero_bloque: int. Cantidad de bloques que hay en la matriz."""
+        Args:
+        numero_bloque: int. Cantidad de bloques que hay en la matriz.
+
+        Returns:
+        None
+        """
         self.x = random.randint(0, numero_bloque - 1)
         self.y = random.randint(0, numero_bloque - 1)
         self.pos = Vector2(self.x,self.y)
+
 
             
 
